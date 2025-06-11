4444787ce77a44a9f1d32d8c52f59c6362b6e0df
PATEL HITKUMAR <hit98987@gmail.com>
2025-06-11 09:39:24 -0400

    add

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Invoices",
+};
+
+const page = () => {
+  return <div>Customer Page</div>;
+};
+
+export default page;
